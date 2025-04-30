0b1de88c27d5edeccf2724c859b84ceb1091d221
ancr2004 <crio@archlinux.localdomain>
2025-04-30 16:45:48 +0000

    added rudimentary api functionalities

---
+++ api.py
@@ -0,0 +1,36 @@
+from fastapi import FastAPI
+from api_funcions import create_conversation generate_audio
+from fastapi.responses import StreamingResponse
+from pydantic import BaseModel
+
+app = FastAPI()
+
+class ConvRequest(BaseModel):
+    audioId: int
+    text: str
+
+# create conversation given context
+# return: conversationID
+
+@app.get("/create")
+def createConversation():
+    convId = create_conversation()
+    return convId
+
+
+# generate audio - gen conversationID 
+# return: audio
+
+@app.post("/returnAudio")
+def generateConversation(payload: ConvRequest):
+    try:
+        tensor = generate_audio(payload.audioId, payload.text)
+        wav_io = generate_wav_file(tensor)
+        return StreamingResponse(
+            wav_io,
+            media_type="audio/wav",
+            headers={"Content-Disposition": f'attachment; filename=\"{request.audio_id}.wav\"'}
+        )
+    except ValueError as e:
+        raise HTTPException(status_code=404, detail=str(e))
+        
